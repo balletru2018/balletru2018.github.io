fdf51cfdbac92ad8538e0b4bbb6dc52f5a594159
balletru2018 <37559433+balletru2018@users.noreply.github.com>
2018-03-22 23:25:00 +0300

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,26 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$name = htmlspecialchars($name);
+$email = htmlspecialchars($email);
+$subject = htmlspecialchars($subject);
+$message = htmlspecialchars($message);
+
+$name = urldecode($name);
+$email = urldecode($email);
+$subject = urldecode($subject);
+$message = urldecode($message);
+
+$name = trim($name);
+$email = trim($email);
+$subject = trim($subject);
+$message = trim($message);
+
+if (mail("balletru.2018@gmail.com", "Сообщение с сайта", "Имя:".$fio.". Электронный адрес: ".$email". Тема: ".$subject". Сообщение: ".$message ,"From: balletru.2018@gmail.com \r\n"))
+ {     echo "Сообщение отправлено"; 
+} else { 
+    echo "При отправке сообщения возникла ошибка";
+}?>
