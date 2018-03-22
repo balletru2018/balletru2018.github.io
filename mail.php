32f6712c3a980e82b1114b09bb8a5c2ba587afae
balletru2018 <37559433+balletru2018@users.noreply.github.com>
2018-03-22 23:58:48 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,17 +1,10 @@
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: *");
-
-error_page 405 =200 @405;
-
-location @405 {
-    include fastcgi.conf;
-}
-
-location ~ \.php(.*) {
-    fastcgi_split_path_info ^(.+\.php)(.*)$;
-    include fastcgi.conf;
-}
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251"/>
+<title>Обратная связь</title>
+</head>
+<body>
 <?php
 $name = $_POST['name'];
 $email = $_POST['email'];
@@ -38,3 +31,5 @@ if (mail("balletru.2018@gmail.com", "Сообщение с сайта", "Имя:
 } else { 
     echo "При отправке сообщения возникла ошибка";
 }?>
+</body>
+</html>
